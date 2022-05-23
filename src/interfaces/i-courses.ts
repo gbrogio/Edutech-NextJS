67038d38f2a9d227c46d3f4bdb154ec23f5524b9
GBrogio <guilhermebrogio.ps@email.com>
2022-05-23 19:11:15 -0300

    chore(types): create courses interfaces

---
+++ src/interfaces/i-courses.ts
@@ -0,0 +1,12 @@
+export interface ICourses {
+  slug: string;
+  name: string;
+  estimatedTime: string;
+}
+
+export interface ICoursesPtBR {
+  slug: string;
+  nome: string;
+  // eslint-disable-next-line camelcase
+  tempo_estimado: string;
+}
