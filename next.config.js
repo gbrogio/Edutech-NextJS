ffd7a59bd734bb1dc6c58f3b53cde4279e8fdf84
GBrogio <guilhermebrogio.ps@email.com>
2022-05-24 17:35:14 -0300

    chore(next): add next.js config file
    add styledComponents compiler, externals domains and extends i18n next configs

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// eslint @typescript-eslint/no-var-requires: off
+const i18next = require('./next-i18next.config.js');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true
+  },
+  images: {
+    domains: ['www.alura.com.br']
+  },
+  ...i18next
+};
+
+module.exports = nextConfig;
