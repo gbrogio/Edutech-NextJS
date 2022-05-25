439f3df592cdf113f0da1e89f539125f7f2dd6f5
GBrogio <guilhermebrogio.ps@email.com>
2022-05-25 17:49:30 -0300

    chore(pages): add my layout in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from 'next/app';
 
+import { Layout } from '@components/Layout';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 };
 
 export default MyApp;
