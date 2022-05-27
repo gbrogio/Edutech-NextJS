9f792394756d069958812fff148e02f45d621810
GBrogio <guilhermebrogio.ps@email.com>
2022-05-27 10:03:22 -0300

    chore: delete FetchStatus, create a new file called FetchStatusIcons and delete HumanIcon file

--- src/styles/components/icons/HumanIcon.ts
+++
@@ -1,8 +0,0 @@
-import { SettingsAccessibility } from '@styled-icons/material/SettingsAccessibility';
-
-import styled from 'styled-components';
-
-export const HumanIcon = styled(SettingsAccessibility)`
-  width: 2.5rem;
-  height: 2.5rem;
-`;
