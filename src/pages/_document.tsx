77af767c7e9773e5929100da9b9a1d735bd50dc8
GBrogio <guilhermebrogio.ps@email.com>
2022-05-30 17:34:05 -0300

    chore(pages, next.js): create _document page, another global page

---
+++ src/pages/_document.tsx
@@ -0,0 +1,72 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  // permit css in js code
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ) as any,
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          {/* METAS */}
+          <meta charSet="UTF-8" />
+
+          {/* FONTS */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap"
+            rel="stylesheet"
+          />
+
+          {/* LOGO */}
+          <link rel="icon" href="" />
+
+          <meta
+            name="description"
+            content="Busque pelos seu cursos de programação aqui - Alunos EduTech -"
+          />
+        </Head>
+
+        <body>
+          <Main />
+
+          {/* SCRIPTS */}
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
