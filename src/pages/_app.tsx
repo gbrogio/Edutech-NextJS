6f7dc84046806e41c4d2f4b7fce759a04b41ae3d
GBrogio <guilhermebrogio.ps@email.com>
2022-05-30 17:44:09 -0300

    chore: create handlers (font size, theme), and implement

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,51 @@
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
+import { useEffect, useState } from 'react';
 
 import { ThemeProvider } from 'styled-components';
 
 import { Layout } from '@components/Layout';
+import { usePersistedStateCookie } from '@hooks/usePersistedStateCookie';
 
 import { defaultTheme } from '@globalStyles/themes/default';
+import { contrastTheme } from '@globalStyles/themes/contrast';
 import { GlobalStyles } from '@globalStyles';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  const [fontSize, setFontSize] = usePersistedStateCookie<'16px' | '18px'>(
+    'FONT_SIZE',
+    '16px',
+  );
+  const [theme, setTheme] = usePersistedStateCookie<'default' | 'contrast'>(
+    'THEME',
+    'default',
+  );
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
-    <ThemeProvider theme={defaultTheme}>
+    <ThemeProvider theme={theme === 'contrast' ? contrastTheme : defaultTheme}>
       <GlobalStyles.Reset />
       <GlobalStyles.Colors />
       <GlobalStyles.Fonts />
       <GlobalStyles.Shadows />
       <GlobalStyles.Others />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      {isMounted && (
+        <Layout
+          accessibilityFunctions={{
+            handleFontSize: () =>
+              setFontSize(fontSize === '16px' ? '18px' : '16px'),
+            handleTheme: () =>
+              setTheme(theme === 'default' ? 'contrast' : 'default'),
+          }}
+        >
+          <Component {...pageProps} />
+        </Layout>
+      )}
     </ThemeProvider>
   );
 };
