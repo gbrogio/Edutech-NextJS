c9c2240b8f091e3838faafc142f03d85049cdc33
GBrogio <guilhermebrogio.ps@email.com>
2022-05-30 17:30:54 -0300

    chore(hooks, types): change types of usePersistedStateCookie

--- src/hooks/usePersistedStateCookie.ts
+++ src/hooks/usePersistedStateCookie.ts
@@ -2,9 +2,9 @@ import { useEffect, useState } from 'react';
 
 import { FetcherCookie } from '@services/fetchers/fetcher-cookie';
 
-export function usePersistedStateCookie(
+export function usePersistedStateCookie<T extends string>(
   key: string,
-  defaultValue: string,
+  defaultValue: T,
 ): [string, React.Dispatch<React.SetStateAction<string>>] {
   const cookie = FetcherCookie();
 
