6ab3504c08e473732f1d72d7ebd0cd432a3ea409
GBrogio <guilhermebrogio.ps@email.com>
2022-06-01 20:21:47 -0300

    chore: implements CoursesProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,13 @@ import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 
 import { LayoutProvider } from '@contexts/Layout';
+import { CoursesProvider } from '@contexts/Courses';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <LayoutProvider>
-    <Component {...pageProps} />
+    <CoursesProvider>
+      <Component {...pageProps} />
+    </CoursesProvider>
   </LayoutProvider>
 );
 
