87984ae4c39d33c1d5746537773fcb726f328f98
GBrogio <guilhermebrogio.ps@email.com>
2022-06-01 22:15:40 -0300

    chore(pages): add title in all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,21 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { appWithTranslation } from 'next-i18next';
 
 import { LayoutProvider } from '@contexts/Layout';
 import { CoursesProvider } from '@contexts/Courses';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <LayoutProvider>
-    <CoursesProvider>
-      <Component {...pageProps} />
-    </CoursesProvider>
-  </LayoutProvider>
+  <>
+    <Head>
+      <title>EduTech - GBrogio</title>
+    </Head>
+    <LayoutProvider>
+      <CoursesProvider>
+        <Component {...pageProps} />
+      </CoursesProvider>
+    </LayoutProvider>
+  </>
 );
 
 export default appWithTranslation(MyApp);
